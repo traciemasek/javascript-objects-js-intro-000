0bf26aac4baabc34d7d54ccbd9901b767beb9b84
Tracie Masek <traciemasek@gmail.com>
2018-12-09 01:50:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,10 +2,14 @@ var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = "songTitle";
+<<<<<<< HEAD
   return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
+=======
+  return playlist
+>>>>>>> d9dc6f3519e2bca2546ca23ec39443530d31c451
 }
